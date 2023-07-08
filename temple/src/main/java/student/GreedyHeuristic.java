816fe81b5653aaca1b44820c8705194065763565
Daniel Verrall <djv3@student.london.ac.uk>
2023-07-08 06:40:19 +0100

    Fixed greedy heuristic estimate to use correct coords for exit

--- temple/src/main/java/student/GreedyHeuristic.java
+++ temple/src/main/java/student/GreedyHeuristic.java
@@ -9,8 +9,8 @@ public class GreedyHeuristic extends Heuristic {
         // calculate manhattan distance between current node and exit node
         int currentColumn = node.getTile().getColumn();
         int currentRow = node.getTile().getRow();
-        int exitColumn = node.getTile().getColumn();
-        int exitRow = node.getTile().getRow();
+        int exitColumn = exitNode.getTile().getColumn();
+        int exitRow = exitNode.getTile().getRow();
         int currentGold = node.getTile().getGold();
         int manhattanDistance = Math.abs(currentColumn - exitColumn) + Math.abs(currentRow - exitRow);
 
