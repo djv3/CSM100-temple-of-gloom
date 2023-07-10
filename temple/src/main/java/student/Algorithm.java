4f3112530d1854e9394224579bb5a2c38108620f
Adam <alc25@student.london.ac.uk>
2023-07-10 11:46:02 +0100

    Added javadoc for Algorithm and EscapeAlgorithm classes

--- temple/src/main/java/student/Algorithm.java
+++ temple/src/main/java/student/Algorithm.java
@@ -4,6 +4,16 @@ import game.Node;
 
 import java.util.List;
 
+/**
+ * An abstract class representing an algorithm to calculate paths in a maze.
+ */
 public abstract class Algorithm {
+    /**
+     * A method to calculate the shortest path between 2 nodes.
+     *
+     * @param _startNode = the node from which to calculate the path
+     * @param _endNode   = the node to which to calculate the path
+     * @return           = the path to take from _startNode to _endNode
+     */
     public abstract List<Node> shortestPath(Node _startNode, Node _endNode);
 }
