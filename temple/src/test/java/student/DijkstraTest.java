cc2aff8ee4aaef8c7208213f4ea550e95fbd5304
Adam <alc25@student.london.ac.uk>
2023-07-11 14:06:17 +0100

    Test methods for EscapeAlgorithm and Dijkstra classes

---
+++ temple/src/test/java/student/DijkstraTest.java
@@ -0,0 +1,117 @@
+package student;
+
+import game.Cavern;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import game.Node;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+class DijkstraTest {
+    Cavern cavern;
+    Dijkstra dijkstra;
+    List<Node> shortestPath;
+    int goldOnMap, goldOnShortestPath, goldOnBestPath;
+    int timeRemaining, timeTakenForShortestPath;
+
+    /**
+     *  Setting seed 12345 with 5 rows and columns will generate the following cavern:
+     *
+     *  1-2-3
+     *  |   |
+     *  4   5-6
+     *  |   |
+     *  7-8-9
+     *
+     *  Tile 1 is the start position. Tile 6 is the exit.
+     *
+     *  Tile 2 has 533 gold, tile 3 has 518 gold and tile 4 has 398 gold. Other tiles have no gold.
+     *
+     *  Edges have the following weights:
+     *  1-2: 11, 2-3:  9
+     *  1-4:  7, 3-5:  6
+     *  5-6: 11
+     *  4-7:  3, 5-9:  7
+     *  7-8: 14, 8-9:  7
+     */
+    @BeforeEach
+    void setUp() {
+        cavern = Cavern.digEscapeCavern(5, 5, 1, 1, new Random(12345));
+
+        // Because we know the layout of the cavern we can hard-code the shortest path
+        shortestPath = new ArrayList<>();
+        shortestPath.add(cavern.getNodeAt(1,2));
+        shortestPath.add(cavern.getNodeAt(1,3));
+        shortestPath.add(cavern.getNodeAt(2,3));
+        shortestPath.add(cavern.getNodeAt(2,4));
+
+        // Because we know the layout of the cavern we can hard-code the gold on the map
+        goldOnMap = 533 + 518 + 398;
+        goldOnShortestPath = 533 + 518;
+        goldOnBestPath = goldOnMap;
+
+        // Because we know the layout of the cavern we can hard-code the time remaining
+        // (using the formula used by the program: timeForShortestPath + 0.3 * (MAX_EDGE_WEIGHT + 1) * cavern.numOpenTiles() / 2)
+        timeRemaining = 58;
+        timeTakenForShortestPath = 37;
+
+        dijkstra = new Dijkstra(cavern.getGraph(), timeRemaining);
+    }
+
+    @Test
+    void testBestPath() {
+        List<Node> actualPath = dijkstra.bestPath(cavern.getNodeAt(1,1), cavern.getEntrance());
+        List<Node> expectedPath = new ArrayList<>();
+        expectedPath.add(cavern.getNodeAt(2,1));
+        expectedPath.add(cavern.getNodeAt(1,1));
+        expectedPath.add(cavern.getNodeAt(1,2));
+        expectedPath.add(cavern.getNodeAt(1,3));
+        expectedPath.add(cavern.getNodeAt(2,3));
+        expectedPath.add(cavern.getNodeAt(2,4));
+        assertEquals(expectedPath, actualPath);
+    }
+
+    @Test
+    void testTotalGoldOnMap() {
+        assertEquals(goldOnMap, dijkstra.totalGoldOnMap());
+    }
+
+    @Test
+    void testTotalGoldOnPath() {
+        assertEquals(goldOnShortestPath, EscapeAlgorithm.totalGoldOnPath(shortestPath));
+    }
+
+    @Test
+    void testTimeTakenToTraversePath() {
+        assertEquals(timeTakenForShortestPath, EscapeAlgorithm.timeTakenToTraversePath(cavern.getNodeAt(1, 1), shortestPath));
+    }
+
+    @Test
+    void testFindPathToClosestNodeWithGold() {
+        // From 1,1 the method should return the path to 2,1 (i.e. just 1 node)
+        List<Node> actualPath = dijkstra.findPathToClosestNodeWithGold(cavern.getNodeAt(1,1), new ArrayList<>());
+        List<Node> expectedPath = new ArrayList<>();
+        expectedPath.add(cavern.getNodeAt(2,1));
+        assertEquals(expectedPath, actualPath);
+
+        // From 3,3 the method should return the path to 1,3 (i.e. 2,3 -> 1,3)
+        actualPath = dijkstra.findPathToClosestNodeWithGold(cavern.getNodeAt(3,3), new ArrayList<>());
+        expectedPath = new ArrayList<>();
+        expectedPath.add(cavern.getNodeAt(2,3));
+        expectedPath.add(cavern.getNodeAt(1,3));
+        assertEquals(expectedPath, actualPath);
+
+        // If all other gold is already collected, the method should route to 2,1 from 3,3
+        List<Node> visitedNodes = new ArrayList<>();
+        visitedNodes.add(cavern.getNodeAt(1,2));
+        visitedNodes.add(cavern.getNodeAt(1,3));
+        actualPath = dijkstra.findPathToClosestNodeWithGold(cavern.getNodeAt(3,3), visitedNodes);
+        expectedPath = new ArrayList<>();
+        expectedPath.add(cavern.getNodeAt(3,2));
+        expectedPath.add(cavern.getNodeAt(3,1));
+        expectedPath.add(cavern.getNodeAt(2,1));
+        assertEquals(expectedPath, actualPath);
+    }
+}
