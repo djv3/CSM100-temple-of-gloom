68a7d0f8190b9b89824000ccb21de8f5d7541da9
an78drew <az147@student.london.ac.uk>
2023-07-11 14:20:00 +0200

    java doc for ExploreAlgorithm class

--- temple/src/main/java/student/ExploreAlgorithm.java
+++ temple/src/main/java/student/ExploreAlgorithm.java
@@ -6,6 +6,11 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 
+/**
+ * Contains three methods to be re-used during implementation of particular search algorithm
+ * {@param entrePoint} is the start node for search
+ */
+
 
 public abstract class ExploreAlgorithm extends Algorithm {
 
@@ -14,6 +19,12 @@ public abstract class ExploreAlgorithm extends Algorithm {
 
     public abstract  NodeA getCurrentNode();
 
+    /**
+     * Converts data<NodeStatus> Collection of neighbours{@param neighbours}  to list of node records NodeA
+     * Uses current node record {@param current} as a parent in record NodeA for all neighbours
+     * @param current  = node which has neighbours
+     * @return list of the records NodeA of  neighbours of the current node
+     */
     public static List<NodeA> getNeighborsA(Collection<NodeStatus> neighbours, NodeA current){
 
         return neighbours.stream().toList().stream()
@@ -24,6 +35,10 @@ public abstract class ExploreAlgorithm extends Algorithm {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Validates if the concrete node {@param m} is in the given list {@param list}
+     * Checks records by nodeID parameter
+     */
     public static boolean is_node_in_list(NodeA m, PriorityQueue<NodeA> list){
         return list.stream().anyMatch(n -> m.nodeStatus().nodeID() == n.nodeStatus().nodeID());
     }
