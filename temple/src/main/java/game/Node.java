4b50366a973cc92ee2fef7005261e95e61a437a8
Daniel Verrall <djv3@student.london.ac.uk>
2023-07-11 16:54:59 +0100

    Ran the formatter, sorry in advance for the whopping diff, but now we can run it before merging everytime

--- temple/src/main/java/game/Node.java
+++ temple/src/main/java/game/Node.java
@@ -5,120 +5,96 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-/**
- * An instance represents a node in the graph --the cavern.
- */
+/** An instance represents a node in the graph --the cavern. */
 public class Node {
-    /**
-     * The least positive fresh identifier available
-     */
-    private static long FRESH_ID = 0;
+  /** The least positive fresh identifier available */
+  private static long FRESH_ID = 0;
 
-    /**
-     * The unique numerical identifier of this Node
-     */
-    private final long id;
-    /**
-     * Represents the edges outgoing from this Node
-     */
-    private final Set<Edge> edges;
-    private final Set<Node> neighbours;
+  /** The unique numerical identifier of this Node */
+  private final long id;
 
-    private final Set<Edge> unmodifiableEdges;
-    private final Set<Node> unmodifiableNeighbours;
+  /** Represents the edges outgoing from this Node */
+  private final Set<Edge> edges;
 
-    /**
-     * Extra state that belongs to this node
-     */
-    private final Tile tile;
+  private final Set<Node> neighbours;
 
-    /* package */ Node(Tile t) {
-        this(FRESH_ID, t);
+  private final Set<Edge> unmodifiableEdges;
+  private final Set<Node> unmodifiableNeighbours;
+
+  /** Extra state that belongs to this node */
+  private final Tile tile;
+
+  /* package */ Node(Tile t) {
+    this(FRESH_ID, t);
+  }
+
+  /** Constructor: an instance with id givenId and tile t */
+  /* package */ Node(long givenId, Tile t) {
+    id = givenId;
+    FRESH_ID = Math.max(FRESH_ID, givenId + 1);
+    edges = new HashSet<>();
+    neighbours = new HashSet<>();
+
+    unmodifiableEdges = Collections.unmodifiableSet(edges);
+    unmodifiableNeighbours = Collections.unmodifiableSet(neighbours);
+
+    tile = t;
+  }
+
+  /** Add edge e to the graph. */
+  /* package */ void addEdge(Edge e) {
+    edges.add(e);
+    neighbours.add(e.getOther(this));
+  }
+
+  /** Return the unique Identifier of this Node. */
+  public long getId() {
+    return id;
+  }
+
+  /**
+   * Returns the Edge of this Node that connects to q. . * Throw an IllegalArgumentException if
+   * there is no such edge.
+   */
+  public Edge getEdge(Node q) {
+    for (Edge e : edges) {
+      if (e.getDest().equals(q)) {
+        return e;
+      }
     }
+    throw new IllegalArgumentException("getEdge: Node must be a neighbour of this Node");
+  }
 
-    /**
-     * Constructor: an instance with id givenId and tile t
-     */
-    /* package */ Node(long givenId, Tile t) {
-        id = givenId;
-        FRESH_ID = Math.max(FRESH_ID, givenId + 1);
-        edges = new HashSet<>();
-        neighbours = new HashSet<>();
+  /** Return an unmodifiable set of Edges leaving this Node. */
+  public Set<Edge> getExits() {
+    return unmodifiableEdges;
+  }
 
-        unmodifiableEdges = Collections.unmodifiableSet(edges);
-        unmodifiableNeighbours = Collections.unmodifiableSet(neighbours);
+  /** Return an unmodifiable set Nodes neighbouring this Node. */
+  public Set<Node> getNeighbours() {
+    return unmodifiableNeighbours;
+  }
 
-        tile = t;
+  /** Return the Tile corresponding to this Node. */
+  public Tile getTile() {
+    return tile;
+  }
+
+  /** Return truee if ob is a Node with the same id as this one. */
+  @Override
+  public boolean equals(Object ob) {
+    if (ob == this) {
+      return true;
     }
-
-    /**
-     * Add edge e to the graph.
-     */
-    /* package */ void addEdge(Edge e) {
-        edges.add(e);
-        neighbours.add(e.getOther(this));
+    if (!(ob instanceof Node)) {
+      return false;
     }
+    return id == ((Node) ob).id;
+  }
 
-    /**
-     * Return the unique Identifier of this Node.
-     */
-    public long getId() {
-        return id;
-    }
-
-    /**
-     * Returns the Edge of this Node that connects to q.
-     * .     * Throw an IllegalArgumentException if there is no such edge.
-     */
-    public Edge getEdge(Node q) {
-        for (Edge e : edges) {
-            if (e.getDest().equals(q)) {
-                return e;
-            }
-        }
-        throw new IllegalArgumentException("getEdge: Node must be a neighbour of this Node");
-    }
-
-    /**
-     * Return an unmodifiable set of Edges leaving this Node.
-     */
-    public Set<Edge> getExits() {
-        return unmodifiableEdges;
-    }
-
-    /**
-     * Return an unmodifiable set Nodes neighbouring this Node.
-     */
-    public Set<Node> getNeighbours() {
-        return unmodifiableNeighbours;
-    }
-
-    /**
-     * Return the Tile corresponding to this Node.
-     */
-    public Tile getTile() {
-        return tile;
-    }
-
-    /**
-     * Return truee if ob is a Node with the same id as this one.
-     */
-    @Override
-    public boolean equals(Object ob) {
-        if (ob == this) {
-            return true;
-        }
-        if (!(ob instanceof Node)) {
-            return false;
-        }
-        return id == ((Node) ob).id;
-    }
-
-    /**
-     * Return a hash code for this node, based solely on its id.
-     */
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+  /** Return a hash code for this node, based solely on its id. */
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 }
--- temple/src/main/java/student/Explorer.java
+++ temple/src/main/java/student/Explorer.java
@@ -1,122 +1,119 @@
 package student;
 
-import java.io.IOException;
-import java.util.*;
-
 import game.EscapeState;
 import game.ExplorationState;
 import game.Node;
 import game.NodeStatus;
+import java.io.IOException;
+import java.util.*;
 
 public class Explorer {
 
-    /**
-     * Explore the cavern, trying to find the orb in as few steps as possible.
-     * Once you find the orb, you must return from the function in order to pick
-     * it up. If you continue to move after finding the orb rather
-     * than returning, it will not count.
-     * If you return from this function while not standing on top of the orb,
-     * it will count as a failure.
-     * <p>
-     * There is no limit to how many steps you can take, but you will receive
-     * a score bonus multiplier for finding the orb in fewer steps.
-     * <p>
-     * At every step, you only know your current tile's ID and the ID of all
-     * open neighbor tiles, as well as the distance to the orb at each of these tiles
-     * (ignoring walls and obstacles).
-     * <p>
-     * To get information about the current state, use functions
-     * getCurrentLocation(),
-     * getNeighbours(), and
-     * getDistanceToTarget()
-     * in ExplorationState.
-     * You know you are standing on the orb when getDistanceToTarget() is 0.
-     * <p>
-     * Use function moveTo(long id) in ExplorationState to move to a neighboring
-     * tile by its ID. Doing this will change state to reflect your new position.
-     * <p>
-     * A suggested first implementation that will always find the orb, but likely won't
-     * receive a large bonus multiplier, is a depth-first search.
-     *
-     * @param state the information available at the current state
-     */
+  /**
+   * Explore the cavern, trying to find the orb in as few steps as possible. Once you find the orb,
+   * you must return from the function in order to pick it up. If you continue to move after finding
+   * the orb rather than returning, it will not count. If you return from this function while not
+   * standing on top of the orb, it will count as a failure.
+   *
+   * <p>There is no limit to how many steps you can take, but you will receive a score bonus
+   * multiplier for finding the orb in fewer steps.
+   *
+   * <p>At every step, you only know your current tile's ID and the ID of all open neighbor tiles,
+   * as well as the distance to the orb at each of these tiles (ignoring walls and obstacles).
+   *
+   * <p>To get information about the current state, use functions getCurrentLocation(),
+   * getNeighbours(), and getDistanceToTarget() in ExplorationState. You know you are standing on
+   * the orb when getDistanceToTarget() is 0.
+   *
+   * <p>Use function moveTo(long id) in ExplorationState to move to a neighboring tile by its ID.
+   * Doing this will change state to reflect your new position.
+   *
+   * <p>A suggested first implementation that will always find the orb, but likely won't receive a
+   * large bonus multiplier, is a depth-first search.
+   *
+   * @param state the information available at the current state
+   */
+  public void explore(ExplorationState state) throws IOException {
+    if (state.getDistanceToTarget() == 0) {
+      return;
+    }
+    // previous node
+    ArrayDeque<Long> savedMoves = new ArrayDeque<>();
+    ArrayDeque<Long> visitedPath = new ArrayDeque<>();
 
-    public void explore(ExplorationState state) throws IOException {
-        if (state.getDistanceToTarget() == 0) {
-            return;
-        }
-        //previous node
-        ArrayDeque<Long> savedMoves = new ArrayDeque<>();
-        ArrayDeque<Long> visitedPath = new ArrayDeque<>();
+    while (state.getDistanceToTarget() > 0) {
+      // push current location onto path taken
+      visitedPath.push(state.getCurrentLocation());
+      // find all neighbors and order with the neighbor closest to the target first
+      List<NodeStatus> neighbors =
+          state.getNeighbours().stream()
+              .filter(neighbor -> !visitedPath.contains(neighbor.nodeID()))
+              .sorted(
+                  Comparator.comparing(NodeStatus::nodeID)
+                      .reversed()
+                      .thenComparing(NodeStatus::compareTo))
+              .toList();
+      // .map(NodeStatus::nodeID).toList();
+      if (!neighbors.isEmpty()) {
+        state.moveTo(neighbors.get(0).nodeID());
+        // add the first neighbor to the saved moves in case we need to visit it again
+        savedMoves.addFirst(state.getCurrentLocation());
+      } else {
+        // no more neighbors, backtrack to the last available neighbor
+        savedMoves.removeFirst();
+        state.moveTo(savedMoves.peekFirst());
+      }
+    }
+  }
 
-       while (state.getDistanceToTarget() > 0) {
-           // push current location onto path taken
-           visitedPath.push(state.getCurrentLocation());
-           // find all neighbors and order with the neighbor closest to the target first
-           List<NodeStatus> neighbors = state.getNeighbours().stream().filter(neighbor -> !visitedPath.contains(neighbor.nodeID()))
-                   .sorted(Comparator.comparing(NodeStatus::nodeID).reversed().thenComparing(NodeStatus::compareTo)).toList();
-                  // .map(NodeStatus::nodeID).toList();
-           if (!neighbors.isEmpty()) {
-               state.moveTo(neighbors.get(0).nodeID());
-               // add the first neighbor to the saved moves in case we need to visit it again
-               savedMoves.addFirst(state.getCurrentLocation());
-           }else{
-               // no more neighbors, backtrack to the last available neighbor
-               savedMoves.removeFirst();
-               state.moveTo(savedMoves.peekFirst());
-           }
-       }
+  /**
+   * Escape from the cavern before the ceiling collapses, trying to collect as much gold as possible
+   * along the way. Your solution must ALWAYS escape before time runs out, and this should be
+   * prioritized above collecting gold.
+   *
+   * <p>You now have access to the entire underlying graph, which can be accessed through
+   * EscapeState. getCurrentNode() and getExit() will return you Node objects of interest, and
+   * getVertices() will return a collection of all nodes on the graph.
+   *
+   * <p>Note that time is measured entirely in the number of steps taken, and for each step the time
+   * remaining is decremented by the weight of the edge taken. You can use getTimeRemaining() to get
+   * the time still remaining, pickUpGold() to pick up any gold on your current tile (this will fail
+   * if no such gold exists), and moveTo() to move to a destination node adjacent to your current
+   * node.
+   *
+   * <p>You must return from this function while standing at the exit. Failing to do so before time
+   * runs out or returning from the wrong location will be considered a failed run.
+   *
+   * <p>You will always have enough time to escape using the shortest path from the starting
+   * position to the exit, although this will not collect much gold.
+   *
+   * @param state the information available at the current state
+   */
+  public void escape(EscapeState state) {
+    int timeRemaining = state.getTimeRemaining();
+    Set<Node> vertices = (Set<Node>) state.getVertices();
 
+    Dijkstra dijkstra = new Dijkstra(vertices, timeRemaining);
+    AStar astar = new AStar(vertices, timeRemaining);
+
+    List<Node> dijkstraRoute = dijkstra.bestPath(state.getCurrentNode(), state.getExit());
+    List<Node> aStarRoute = astar.bestPath(state.getCurrentNode(), state.getExit());
+    List<Node> escapeRoute;
+
+    if (EscapeAlgorithm.totalGoldOnPath(dijkstraRoute)
+        > EscapeAlgorithm.totalGoldOnPath(aStarRoute)) {
+      System.out.println("Dijkstra route is better");
+      escapeRoute = dijkstraRoute;
+    } else {
+      System.out.println("A* route is better");
+      escapeRoute = aStarRoute;
     }
 
-
-    /**
-     * Escape from the cavern before the ceiling collapses, trying to collect as much
-     * gold as possible along the way. Your solution must ALWAYS escape before time runs
-     * out, and this should be prioritized above collecting gold.
-     * <p>
-     * You now have access to the entire underlying graph, which can be accessed through EscapeState.
-     * getCurrentNode() and getExit() will return you Node objects of interest, and getVertices()
-     * will return a collection of all nodes on the graph.
-     * <p>
-     * Note that time is measured entirely in the number of steps taken, and for each step
-     * the time remaining is decremented by the weight of the edge taken. You can use
-     * getTimeRemaining() to get the time still remaining, pickUpGold() to pick up any gold
-     * on your current tile (this will fail if no such gold exists), and moveTo() to move
-     * to a destination node adjacent to your current node.
-     * <p>
-     * You must return from this function while standing at the exit. Failing to do so before time
-     * runs out or returning from the wrong location will be considered a failed run.
-     * <p>
-     * You will always have enough time to escape using the shortest path from the starting
-     * position to the exit, although this will not collect much gold.
-     *
-     * @param state the information available at the current state
-     */
-    public void escape(EscapeState state) {
-        int timeRemaining = state.getTimeRemaining();
-        Set<Node> vertices = (Set<Node>) state.getVertices();
-
-        Dijkstra dijkstra = new Dijkstra(vertices, timeRemaining);
-        AStar astar = new AStar(vertices, timeRemaining);
-
-        List<Node> dijkstraRoute = dijkstra.bestPath(state.getCurrentNode(), state.getExit());
-        List<Node> aStarRoute = astar.bestPath(state.getCurrentNode(), state.getExit());
-        List<Node> escapeRoute;
-
-        if (EscapeAlgorithm.totalGoldOnPath(dijkstraRoute) > EscapeAlgorithm.totalGoldOnPath(aStarRoute)) {
-            System.out.println("Dijkstra route is better");
-            escapeRoute = dijkstraRoute;
-        } else {
-            System.out.println("A* route is better");
-            escapeRoute = aStarRoute;
-        }
-
-        while (escapeRoute.size() > 0) {
-            if (state.getCurrentNode().getTile().getGold() > 0) {
-                state.pickUpGold();
-            }
-            state.moveTo(escapeRoute.remove(0));
-        }
+    while (escapeRoute.size() > 0) {
+      if (state.getCurrentNode().getTile().getGold() > 0) {
+        state.pickUpGold();
+      }
+      state.moveTo(escapeRoute.remove(0));
     }
+  }
 }
